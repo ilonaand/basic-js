2446f555198a202e38452c3e76694621684993af
ilonaand <ilonaand@gmail.com>
2023-12-23 22:39:13 +0300

    fix:30

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let addition = '';
+  let additionSeparator='';
+
+  if (options.addition) addition = options.addition.toString();
+  if (options.additionSeparator) additionSeparator = options.additionSeparator.toString();
+  addition = addition + additionSeparator;
+
+  if (options.additionRepeatTimes) addition = 
+    addition.repeat(Number(options.additionRepeatTimes));
+
+  if (options.additionSeparator) addition = addition.slice(0, -additionSeparator.toString().length);
+ 
+  let newstr = str;
+  let separator = '+';
+  if (options.separator) separator = options.separator.toString()
+   newstr = newstr + addition + separator; 
+
+  if (options.repeatTimes) newstr = newstr.repeat(Number(options.repeatTimes));
+  newstr = newstr.slice(0, -separator.length)
+  return newstr;
 }
 
 module.exports = {
